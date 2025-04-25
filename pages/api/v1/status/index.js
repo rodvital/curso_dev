a679ec05d12666eac046e86c2c1e57d6a3d92e46
Rodrigo Vital <67170544+rodvital@users.noreply.github.com>
2025-04-25 12:12:12 +0000

    add jsconfig com basURL

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1 as sum");
+  const result = await database.query("select 4+1 as sum");
   console.log(result.rows);
   response.status(200).json({ chave: "são acima da média mesmo" });
 }
